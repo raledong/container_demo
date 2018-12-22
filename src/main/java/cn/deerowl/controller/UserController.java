1834815cc4f57b5c9b1ccad5b3dd169ec4540584
raledong <raledong@gmail.com>
2018-12-22 14:18:31 +0800

    脚本更新：上传至私有仓库

--- src/main/java/cn/deerowl/controller/UserController.java
+++ src/main/java/cn/deerowl/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.deerowl.controller;
 
 import cn.deerowl.dao.UserMapper;
 import cn.deerowl.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,16 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("/user")
-public class HelloWorldController{
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserMapper userMapper;
 
     @GetMapping("/")
     public List<User> getAll(){
+        logger.info("返回所有的用户");
         return userMapper.getAllUsers();
     }
 
@@ -26,6 +31,7 @@ public class HelloWorldController{
         User user = new User();
         user.setName("rale");
         userMapper.add(user);
+        logger.info("添加一个新用户");
         return "添加新用户 rale 至数据库中";
     }
 }
--- startup.sh
+++ startup.sh
@@ -2,7 +2,9 @@
 
 mvn clean install dockerfile:build -Dmaven.test.skip=true
 
-CONTAINER_NAME=demo
+#清除当前已有的容器
+CONTAINER_NAME=container_demo
+CONTAINER_VERSION=1.0
 CONTAINER_ALREADY_EXIST="$(docker container ls -a | grep ${CONTAINER_NAME})"
 if [ "${CONTAINER_ALREADY_EXIST}" ];
 then
@@ -11,9 +13,21 @@ then
     docker container rm ${CONTAINER_NAME}
     docker rmi $(docker images -q -f dangling=true)
 fi
-docker run \
-    --name ${CONTAINER_NAME} \
-    -d \
-    -p 8081:8081 \
-    --link=demo_db:db \
-    demo/container_demo
+
+NEXUS_REPO_ADDR=10.1.1.243:18082
+IMAGE_EXIST="$(docker images ls | grep ${NEXUS_REPO_ADDR}/${CONTAINER_NAME})"
+if [ "${IMAGE_EXIST}" ];
+then
+     echo "image ${NEXUS_REPO_ADDR}/${CONTAINER_NAME} already exists"
+     docker rmi ${NEXUS_REPO_ADDR}/${CONTAINER_NAME}
+     docker rmi $(docker images -q -f dangling=true)
+fi
+
+docker tag demo/${CONTAINER_NAME} ${NEXUS_REPO_ADDR}/${CONTAINER_NAME}:${CONTAINER_VERSION}
+docker push ${NEXUS_REPO_ADDR}/${CONTAINER_NAME}:${CONTAINER_VERSION}
+#docker run \
+#    --name ${CONTAINER_NAME} \
+#    -d \
+#    -p 8081:8081 \
+#    --link=demo_db:db \
+#    demo/container_demo
