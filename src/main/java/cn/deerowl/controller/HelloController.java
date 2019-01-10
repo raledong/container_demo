93a2d2e8b30c261420660dabcfcd1fc40cecd377
raledong <raledong@gmail.com>
2019-01-10 14:45:40 +0800

    1.0版本

--- src/main/java/cn/deerowl/controller/HelloController.java
+++ src/main/java/cn/deerowl/controller/HelloController.java
@@ -1,4 +1,15 @@
 package cn.deerowl.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HelloController {
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "hello world!";
+    }
 }
